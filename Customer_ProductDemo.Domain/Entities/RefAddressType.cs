c31c26c49c6e70a1fc63ef3c52e4d26c9df4ae12
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-05-15 15:50:17 +0100

    Added 5 classes

---
+++ Customer_ProductDemo.Domain/Entities/RefAddressType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_ProductDemo.Domain.Entities
+{
+    public class RefAddressType
+    {
+        [Required(ErrorMessage = "Address Type code is a required field.")]
+        public string? AddressTypeCode { get; set; }
+
+        [Required(ErrorMessage = "Address is a required field.")]
+        [MaxLength(100)]
+        public string? Address { get; set; }
+
+    }
+}
---
+++ Customer_ProductDemo.Domain/Entities/RefOrderStatusCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_ProductDemo.Domain.Entities
+{
+    public class RefOrderStatusCode
+    {
+        public int OrderStatusCode { get; set; }
+        public string? Order_Status_Description { get; set; }
+
+    }
+}
